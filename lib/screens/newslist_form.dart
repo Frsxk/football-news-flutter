e3a167656ff9705ac7e78a2bf7ae4b1d5de8a8b1
Faza <muhammad.faza44@ui.ac.id>
2025-11-12 14:54:44 +0700

    Tutorial 8

--- lib/screens/newslist_form.dart
+++ lib/screens/newslist_form.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:football_news/widgets/left_drawer.dart';
+import 'dart:convert';
+import 'package:provider/provider.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:football_news/screens/menu.dart';
 
 class NewsFormPage extends StatefulWidget {
   const NewsFormPage({super.key});
@@ -28,6 +32,7 @@ class _NewsFormPageState extends State<NewsFormPage> {
 
   @override
   Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
     return Scaffold(
       appBar: AppBar(
         title: const Center(
@@ -192,15 +197,44 @@ class _NewsFormPageState extends State<NewsFormPage> {
                               actions: [
                                 TextButton(
                                   child: const Text('OK'),
-                                  onPressed: () {
-                                    Navigator.pop(context);
+                                  onPressed: () async {
+                                    if (_formKey.currentState!.validate()) {
+                                      final response = await request.postJson(
+                                        "http://localhost:8000/create-flutter/",
+                                        jsonEncode({
+                                          "title": _title,
+                                          "content": _content,
+                                          "thumbnail": _thumbnail,
+                                          "category": _category,
+                                          "is_featured": _isFeatured,
+                                        }),
+                                      );
+                                      if (context.mounted) {
+                                        if (response['status'] == 'success') {
+                                          ScaffoldMessenger.of(context)
+                                              .showSnackBar(const SnackBar(
+                                            content: Text("News successfully saved!"),
+                                          ));
+                                          Navigator.pushReplacement(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) => MyHomePage()),
+                                          );
+                                        } else {
+                                          ScaffoldMessenger.of(context)
+                                              .showSnackBar(const SnackBar(
+                                            content: Text("Something went wrong, please try again."),
+                                          ));
+                                        }
+                                      }
+                                      _formKey.currentState!.reset();
+                                    }
                                   },
                                 ),
                               ],
                             );
                           },
                         );
-                        _formKey.currentState!.reset();
                       }
                     },
                     child: const Text(
--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:football_news/screens/menu.dart';
 import 'package:football_news/screens/newslist_form.dart';
+import 'package:football_news/screens/news_entry_list.dart';
 
 class LeftDrawer extends StatelessWidget {
   const LeftDrawer({super.key});
@@ -62,14 +63,14 @@ class LeftDrawer extends StatelessWidget {
           ),
           // Buat ListTile baru untuk ke halaman melihat news
           ListTile(
-            leading: const Icon(Icons.article_outlined),
-            title: const Text('View News'),
-            // Bagian redirection ke NewsListPage
+            leading: const Icon(Icons.add_reaction_rounded),
+            title: const Text('News List'),
             onTap: () {
-              /*
-              TODO: Buatlah routing ke NewsListPage di sini,
-              setelah halaman NewsListPage sudah dibuat.
-              */
+              // Route to news list page
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const NewsEntryListPage()),
+              );
             },
           ),
         ],
